f4863bbb5b3b210a09ece88b53390d51f0c62e18
yasharsanayee <yasharsanayee@gmail.com>
2023-12-14 18:02:47 +0330

    routes and basic styles refactor

--- ticket-challenge/src/app/app-routing.module.ts
+++ ticket-challenge/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanComponent } from './plan/plan.component';
+import { SalonsListComponent } from './salons-list/salons-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: SalonsListComponent,
+    },
+    {
+        path: ':map_id',
+        component: PlanComponent,
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: '',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
