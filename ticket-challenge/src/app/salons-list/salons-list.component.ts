de3b2e7194b4a207a2ba3bf1a7db53b6bb6bc54c
yasharsanayee <yasharsanayee@gmail.com>
2023-12-14 17:15:34 +0330

    salons list template

--- ticket-challenge/src/app/salons-list/salons-list.component.ts
+++ ticket-challenge/src/app/salons-list/salons-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from '../services/map.service';
+import { ReactiveState } from '../utils/reactive-state/reactive-state';
+import { MapResponse } from '../utils/types/map';
 
 @Component({
-  selector: 'app-salons-list',
-  templateUrl: './salons-list.component.html',
-  styleUrls: ['./salons-list.component.scss']
+    selector: 'app-salons-list',
+    templateUrl: './salons-list.component.html',
+    styleUrls: ['./salons-list.component.scss'],
 })
 export class SalonsListComponent implements OnInit {
+    map: ReactiveState<MapResponse>;
 
-  constructor() { }
+    constructor(private _mapService: MapService) {
+        this.map = this._mapService.map;
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.map.update();
+    }
 }
