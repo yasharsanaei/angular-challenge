0054db482250f97b1ff8410c219698428668262f
yasharsanayee <yasharsanayee@gmail.com>
2023-12-15 18:29:53 +0330

    plan basic implementation

--- ticket-challenge/src/app/plan/plan.component.ts
+++ ticket-challenge/src/app/plan/plan.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, Subject, takeUntil } from 'rxjs';
+import { MapService } from '../services/map.service';
+import { ReactiveState } from '../utils/reactive-state/reactive-state';
+import { MapDetailResponse } from '../utils/types/map';
 
 @Component({
-  selector: 'app-plan',
-  templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.scss']
+    selector: 'app-plan',
+    templateUrl: './plan.component.html',
+    styleUrls: ['./plan.component.scss'],
 })
-export class PlanComponent implements OnInit {
+export class PlanComponent implements OnInit, OnDestroy {
+    #onDestroy$: Subject<void> = new Subject<void>();
+    #mapId?: string;
+    mapDetail: ReactiveState<MapDetailResponse>;
 
-  constructor() { }
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router,
+        private _mapService: MapService,
+    ) {
+        this.mapDetail = this._mapService.mapDetail;
+    }
 
-  ngOnInit(): void {
-    // You should get the plan array from the: base-url/map/<map_id>
-  }
+    ngOnInit(): void {
+        this._activatedRoute.params
+            .pipe(
+                takeUntil(this.#onDestroy$),
+                map((params) => params['map_id']),
+            )
+            .subscribe((mapId) => {
+                this.#mapId = mapId;
+                if (this.#mapId) this.mapDetail.update(this.#mapId);
+                else this._router.navigate(['']);
+            });
+        // You should get the plan array from the: base-url/map/<map_id>
+    }
 
+    ngOnDestroy(): void {
+        this.#onDestroy$.next();
+        this.#onDestroy$.complete();
+    }
 }
